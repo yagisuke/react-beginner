0688c385528af917e60e27bd0fe66e350a187aa5
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-03-28 23:19:42 +0900

    modify excel component

--- app/bundle.js
+++ app/bundle.js
@@ -241,6 +241,8 @@ var Excel = _react2.default.createClass({
     initialData: _react2.default.PropTypes.arrayOf(_react2.default.PropTypes.arrayOf(_react2.default.PropTypes.string))
   },
 
+  _preFilterData: null,
+
   getInitialState: function getInitialState() {
     return {
       data: this.props.initialData,
@@ -267,47 +269,134 @@ var Excel = _react2.default.createClass({
     });
   },
 
-  render: function render() {
-    var state = this.state;
+  _toggleFilter: function _toggleFilter() {
+    if (this.state.isfilter) {
+      this.setState({
+        data: this._preFilterData,
+        isfilter: false
+      });
+      this._preFilterData = null;
+    } else {
+      this._preFilterData = this.state.data;
+      this.setState({ isfilter: true });
+    }
+  },
+
+  _filter: function _filter(e) {
+    var inputtext = e.target.value.toLowerCase();
+
+    if (!inputtext) {
+      this.setState({ data: this._preFilterData });
+      return;
+    }
+
+    var index = e.target.dataset.index;
+    var filterdata = this._preFilterData.filter(function (rowitem) {
+      return rowitem[index].toString().toLowerCase().indexOf(inputtext) > -1;
+    });
+    this.setState({ data: filterdata });
+  },
+
+  _download: function _download(format, ev) {
+    var contents = format === 'json' ? JSON.stringify(this.state.data) : this.state.data.reduce(function (result, row) {
+      return result + row.reduce(function (rowresult, cell, idx) {
+        return rowresult + '"' + cell.replace(/"/g, '""') + '"' + (idx < row.length - 1 ? ',' : '');
+      }, '') + "\n";
+    }, '');
+
+    var URL = window.URL || window.webkitURL;
+    var blob = new Blob([contents], { type: 'text/' + format });
+    ev.target.href = URL.createObjectURL(blob);
+    ev.target.download = format + '-data.' + format;
+  },
+
+  _renderFilter: function _renderFilter() {
+    if (!this.state.isfilter) {
+      return null;
+    }
+    return _react2.default.createElement(
+      'tr',
+      { onChange: this._filter },
+      this.props.headers.map(function (_ignore, index) {
+        return _react2.default.createElement(
+          'td',
+          { key: index },
+          _react2.default.createElement('input', { type: 'text', 'data-index': index })
+        );
+      })
+    );
+  },
+
+  _renderToolbox: function _renderToolbox() {
     return _react2.default.createElement(
       'div',
-      { className: 'Excel' },
+      { className: "Toolbox" },
       _react2.default.createElement(
-        'table',
-        null,
+        'button',
+        { onClick: this._toggleFilter },
+        '\u691C\u7D22'
+      ),
+      _react2.default.createElement(
+        'a',
+        { onClick: this._download.bind(this, 'json'), href: 'data.json' },
+        'JSON\u30C0\u30A6\u30F3\u30ED\u30FC\u30C9'
+      ),
+      _react2.default.createElement(
+        'a',
+        { onClick: this._download.bind(this, 'csv'), href: 'data.csv' },
+        'CSV\u30C0\u30A6\u30F3\u30ED\u30FC\u30C9'
+      )
+    );
+  },
+
+  _renderTable: function _renderTable() {
+    var state = this.state;
+
+    return _react2.default.createElement(
+      'table',
+      { className: "Excel" },
+      _react2.default.createElement(
+        'thead',
+        { onClick: this._sort },
         _react2.default.createElement(
-          'thead',
-          { onClick: this._sort },
-          _react2.default.createElement(
-            'tr',
-            null,
-            this.props.headers.map(function (title, index) {
-              return _react2.default.createElement(
-                'th',
-                { key: index },
-                state.sortby === index ? state.isdesc ? title + ' \u2191' : title + ' \u2193' : title
-              );
-            })
-          )
-        ),
-        _react2.default.createElement(
-          'tbody',
+          'tr',
           null,
-          state.data.map(function (rowitems, rowindex) {
+          this.props.headers.map(function (title, index) {
             return _react2.default.createElement(
-              'tr',
-              { key: rowindex },
-              rowitems.map(function (name, index) {
-                return _react2.default.createElement(
-                  'td',
-                  { key: index },
-                  name
-                );
-              })
+              'th',
+              { key: index },
+              state.sortby === index ? state.isdesc ? title + ' \u2191' : title + ' \u2193' : title
             );
           })
         )
+      ),
+      _react2.default.createElement(
+        'tbody',
+        null,
+        this._renderFilter(),
+        state.data.map(function (rowitems, rowindex) {
+          return _react2.default.createElement(
+            'tr',
+            { key: rowindex },
+            rowitems.map(function (name, index) {
+              return _react2.default.createElement(
+                'td',
+                { key: index },
+                name
+              );
+            })
+          );
+        })
       )
+    );
+  },
+
+  render: function render() {
+    return _react2.default.createElement(
+      'div',
+      null,
+      this._renderToolbox(),
+      this._renderTable()
     );
   }
 });
--- app/js/source/components/Excel.js
+++ app/js/source/components/Excel.js
@@ -14,6 +14,8 @@ var Excel = React.createClass({
     )
   },
 
+  _preFilterData: null,
+
   getInitialState: function() {
     return {
       data: this.props.initialData,
@@ -40,48 +42,119 @@ var Excel = React.createClass({
     });
   },
 
-  render: function() {
-    var state = this.state;
+  _toggleFilter: function() {
+    if (this.state.isfilter) {
+      this.setState({
+        data: this._preFilterData,
+        isfilter: false
+      });
+      this._preFilterData = null;
+    } else {
+      this._preFilterData = this.state.data;
+      this.setState({isfilter: true});
+    }
+  },
+
+  _filter: function(e) {
+    var inputtext = e.target.value.toLowerCase();
+
+    if (!inputtext) {
+      this.setState({data: this._preFilterData});
+      return;
+    }
+
+    var index = e.target.dataset.index;
+    var filterdata = this._preFilterData.filter(function(rowitem) {
+      return rowitem[index].toString().toLowerCase().indexOf(inputtext) > -1;
+    });
+    this.setState({data: filterdata});
+  },
+
+  _download: function(format, ev) {
+    var contents = format === 'json' ? JSON.stringify(this.state.data) : this.state.data.reduce(function(result, row) {
+      return result + row.reduce(function(rowresult, cell, idx) {
+        return rowresult + '"' + cell.replace(/"/g, '""') + '"' + (idx < row.length - 1 ? ',' : '');
+      }, '') + "\n";
+    }, '');
+
+    var URL = window.URL || window.webkitURL;
+    var blob = new Blob([contents], {type: 'text/' + format});
+    ev.target.href = URL.createObjectURL(blob);
+    ev.target.download = format + '-data.' + format;
+  },
+
+  _renderFilter: function() {
+    if (!this.state.isfilter) {
+      return null;
+    }
     return (
-      <div className="Excel">
-        <table>
-          <thead onClick={this._sort}>
-            <tr>
-              {
-                this.props.headers.map(function(title, index) {
-                  return (
-                    <th key={index}>
-                      {
-                        state.sortby === index ?
-                          state.isdesc ?
-                            title + ' \u2191' :
-                            title + ' \u2193' :
-                          title
-                      }
-                    </th>
-                  );
-                })
-              }
-            </tr>
-          </thead>
-          <tbody>
+      <tr onChange={this._filter}>
+        {this.props.headers.map(function(_ignore, index) {
+          return <td key={index}><input type="text" data-index={index}/></td>;
+        })}
+      </tr>
+    );
+  },
+
+  _renderToolbox: function() {
+    return (
+      <div className={"Toolbox"}>
+        <button onClick={this._toggleFilter}>検索</button>
+        <a onClick={this._download.bind(this, 'json')} href="data.json">JSONダウンロード</a>
+        <a onClick={this._download.bind(this, 'csv')} href="data.csv">CSVダウンロード</a>
+      </div>
+    );
+  },
+
+  _renderTable: function() {
+    var state = this.state;
+
+    return (
+      <table className={"Excel"}>
+        <thead onClick={this._sort}>
+          <tr>
             {
-              state.data.map(function(rowitems, rowindex) {
+              this.props.headers.map(function(title, index) {
                 return (
-                  <tr key={rowindex}>
+                  <th key={index}>
                     {
-                      rowitems.map(function(name, index) {
-                        return (
-                          <td key={index}>{name}</td>
-                        );
-                      })
+                      state.sortby === index ?
+                        state.isdesc ?
+                          title + ' \u2191' :
+                          title + ' \u2193' :
+                        title
                     }
-                  </tr>
+                  </th>
                 );
               })
             }
-          </tbody>
-        </table>
+          </tr>
+        </thead>
+        <tbody>
+          {this._renderFilter()}
+          {
+            state.data.map(function(rowitems, rowindex) {
+              return (
+                <tr key={rowindex}>
+                  {
+                    rowitems.map(function(name, index) {
+                      return <td key={index}>{name}</td>;
+                    })
+                  }
+                </tr>
+              );
+            })
+          }
+        </tbody>
+      </table>
+    );
+  },
+
+  render: function() {
+    return (
+      <div>
+        {this._renderToolbox()}
+        {this._renderTable()}
       </div>
     );
   }
