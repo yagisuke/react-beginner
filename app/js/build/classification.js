c45387d86c8e1cf32f8e52498bdc967da31669bc
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-03-30 23:37:25 +0900

    delete build file

--- app/js/build/classification.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = {
-  grapes: ['ウインク', 'オーロラブラック', 'オリエンタルスター', 'キャンベル・アーリー', '巨峰', '紫玉', 'スチューベン', '大峰', '高墨', 'ナガノパープル', 'ピオーネ', '藤稔', 'ブラックオリンピア', 'ブラックビート', 'マスカット・ベリーA']
-};
