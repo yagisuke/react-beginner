5993c91c09dadb61bb76f1ca402ff27db465e681
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-04-03 11:17:26 +0900

    add test pattern for forminput

--- whinepad2/js/test/__tests__/FormInput.test.js
+++ whinepad2/js/test/__tests__/FormInput.test.js
@@ -13,37 +13,37 @@ describe('フォームインプットのテスト', () => {
   it('返却値のテスト', () => {
     expect(
       TestUtils.findRenderedDOMComponentWithTag(
-        TestUtils.renderIntoDocument(<FormInput/>),
+        TestUtils.renderIntoDocument(<FormInput />),
         'input',
       ).type
     ).toBe('text');
 
     expect(
       TestUtils.findRenderedDOMComponentWithTag(
-        TestUtils.renderIntoDocument(<FormInput type="year"/>),
+        TestUtils.renderIntoDocument(<FormInput type="year" />),
         'input',
       ).type
     ).toBe('number');
 
     expect(
       TestUtils.findRenderedDOMComponentWithTag(
-        TestUtils.renderIntoDocument(<FormInput type="suggest" options={['r', 'g', 'b']}/>),
+        TestUtils.renderIntoDocument(<FormInput type="rating" id="rating" />),
+        'input',
+      ).type
+    ).toBe('hidden');
+
+    expect(
+      TestUtils.findRenderedDOMComponentWithTag(
+        TestUtils.renderIntoDocument(<FormInput type="suggest" options={['r', 'g', 'b']} />),
         'datalist',
       ).id
     ).toBeTruthy();
 
     expect(
       TestUtils.findRenderedDOMComponentWithTag(
-        TestUtils.renderIntoDocument(<FormInput type="text"/>),
+        TestUtils.renderIntoDocument(<FormInput type="text" />),
         'textarea',
       ).nodeName
     ).toBe('TEXTAREA');
-  });
-
-  it('returns input value', () => {
-    let input = TestUtils.renderIntoDocument(<FormInput type="year"/>);
-    expect(input.getValue()).toBe(String(new Date().getFullYear()));
-    input = TestUtils.renderIntoDocument(<FormInput type="rating" defaultValue="3"/>);
-    expect(input.getValue()).toBe(3);
   });
 });
